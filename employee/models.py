362e9fe84e557a42a1c9a2786917155b623c61f7
sodiqdevpython <sodiqdevpython@gmail.com>
2024-05-22 13:48:08 +0500

    organization va employee ortasida aloqa ornatildi

--- employee/models.py
+++ employee/models.py
@@ -1,5 +1,5 @@
 from django.db import models as m
-
+from organization.models import Organization
 # Create your models here.
 class Employee(m.Model):    #xodim uchun class/model yaratildi va malumotlari olindi
     first_name = m.CharField(max_length=50)
@@ -18,5 +18,6 @@ class Employee(m.Model):    #xodim uchun class/model yaratildi va malumotlari ol
     illness = m.CharField(max_length=100)
     family_health = m.CharField(max_length=100)
     underages = m.IntegerField()
+    organization = m.ForeignKey(Organization, on_delete=m.CASCADE, null=True)
     def __str__(self):  #obyektga string qaytaradi, shu obyekta o'tgan xodim nomini qaytaradi
         return f"{self.first_name} {self.last_name}"
--- organization/models.py
+++ organization/models.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import AbstractUser
-from employee.models import Employee
+#from employee.models import Employee
 from django.db import models as m
 class Organization(AbstractUser):
     name = m.CharField(max_length=200)
     email = m.EmailField(unique=True)
-    money = m.IntegerField()
-    monthly_money = m.IntegerField()
-    employees = m.ForeignKey(Employee, on_delete=m.CASCADE, related_name="employee_for_organization")
-      
-    USERNAME_FIELD = 'email'  
+    money = m.IntegerField(default=0)
+    monthly_money = m.IntegerField(default=0)
+    #employees = m.ForeignKey(Employee, on_delete=m.CASCADE, related_name="employee_for_organization", null=True)
+    username = m.CharField(max_length=100, unique=True)
+    USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []  
 
     def __str__(self):
