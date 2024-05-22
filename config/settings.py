af8faacbbf2a7e1f7d1290fc90ddd4eb4dec96c6
Пользователь <sodiqdevpython@gmail.com>
2024-05-22 16:37:44 +0500

    modellar to'g'irlandi

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "employee",
+    'employee',
     'organization'
 ]
 
@@ -100,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-AUTH_USER_MODEL = 'organization.Organization'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
