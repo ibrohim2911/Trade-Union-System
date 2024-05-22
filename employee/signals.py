56d98e42846a68b0ea8c06594850a869eb4889a1
sodiqdevpython <sodiqdevpython@gmail.com>
2024-05-22 23:32:38 +0500

    Model yangilandi qo'shimcha narsalar qo'shildi

--- employee/signals.py
+++ employee/signals.py
@@ -1,15 +1,15 @@
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from .models import Employee
+# from django.dispatch import receiver
+# from django.db.models.signals import post_save
+# from .models import Employee
 
-@receiver(post_save, sender=Employee)
-def calculate_point(sender, instance, created, *args, **kwargs):
-    if created:
-        print(instance.first_name)
-        print(instance.is_married)
-        print(instance.handicapped)
-        print(is_role.role)
-        try:
-            print(instance.organization)
-        except:
-            pass
+# @receiver(post_save, sender=Employee)
+# def calculate_point(sender, instance, created, *args, **kwargs):
+#     if created:
+#         print(instance.first_name)
+#         print(instance.is_married)
+#         print(instance.handicapped)
+#         print(instance.is_role)
+#         try:
+#             print(instance.organization)
+#         except:
+#             pass
