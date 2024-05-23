37e87f5008f5b847e248dcb9b0932c741a4e9952
sodiqdevpython <sodiqdevpython@gmail.com>
2024-05-23 05:02:10 +0500

    application app yaratildi va model, admin togirlandi

--- config/settings.py
+++ config/settings.py
@@ -31,14 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employee',
-    'organization'
+
+    "phonenumber_field",
+
+    "employee",
+    "organization",
+    "application"
 ]
 
 MIDDLEWARE = [
@@ -117,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- employee/admin.py
+++ employee/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import Employee
-# Register your models here.
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['first_name']
-    search_fields = ['first_name', "passport_id"]
-admin.site.register(Employee, EmployeeAdmin)
+from employee import models
+
+
+class UnderagesFiles(admin.TabularInline):
+    fk_name = "underages"
+    model = models.UnderageFile
+    extra = 1
+
+
+@admin.register(models.Underages)
+class UnderagesAdmin(admin.ModelAdmin):
+    list_display = ("pk", "full_name")
+
+    inlines = [UnderagesFiles]
+
+
+admin.site.register(models.Role)
+admin.site.register(models.FamilyStatus)
+admin.site.register(models.Handicapped)
+admin.site.register(models.Illness)
+admin.site.register(models.Employee)
